a45b891fe1aa27e1f5be9a68e062259ab9d098f9
danmed <danmed@gmail.com>
2023-02-23 23:59:23 +0000

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -3,13 +3,14 @@ apt update && apt upgrade && apt install dialog -y
 HEIGHT=15
 WIDTH=40
 CHOICE_HEIGHT=4
-BACKTITLE="Backtitle here"
-TITLE="Title here"
+BACKTITLE="Dan's Menu"
+TITLE="Install Scripts"
 MENU="Choose one of the following options:"
 
-OPTIONS=(1 "Option 1"
-         2 "Option 2"
-         3 "Option 3")
+OPTIONS=(1 "Install Docker"
+         2 "Install Portainer Agent"
+         3 "Enable SSH Root Access"
+         4 "All")
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
@@ -22,12 +23,15 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         1)
-            echo "You chose Option 1"
+            wget -O - https://dlnk.co.uk/docker | bash
             ;;
         2)
-            echo "You chose Option 2"
+            wget -O - https://dlnk.co.uk/portaineragent | bash
             ;;
         3)
-            echo "You chose Option 3"
+            wget -O - https://dlnk.co.uk/ssh | bash
+            ;;
+        4)
+            wget -O - https://dlnk.co.uk/all | bash
             ;;
 esac
