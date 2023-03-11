66204af59773018ff7fa9e6ffbeb2c27569a4b60
danmed <danmed@gmail.com>
2023-03-11 21:33:07 +0000

    Update exporter.sh

--- exporter.sh
+++ exporter.sh
@@ -1,1 +1,1 @@
-docker run -v /var/run/docker.sock:/var/run/docker.sock:ro -v /etc/localtime:/etc/localtime:ro --name dcw-exporter -e TOKEN=9fvaKTqMrx8Zw4 -e MAIN_DCW=192.168.2.57 palleri/dcw-exporter:latest
+docker run -v /var/run/docker.sock:/var/run/docker.sock:ro -v /etc/localtime:/etc/localtime:ro --name dcw-exporter --restart=always -e TOKEN=9fvaKTqMrx8Zw4 -e MAIN_DCW=192.168.2.57 palleri/dcw-exporter:latest
